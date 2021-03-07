03c9987ffea7cd580d75f43d9f6e3b8c973f33db
Hhpon <624791164@qq.com>
2021-03-07 21:16:13 +0800

    modify float-calculator

--- float-calculator/float-calculator.js
+++ float-calculator/float-calculator.js
@@ -1,12 +1,33 @@
+"use strict";
+function strip(num, precision) {
+    if (precision === void 0) { precision = 15; }
+    return +parseFloat(Number(num).toPrecision(precision));
+}
+function digitLength(num) {
+    var eSplit = num.toString().split(/[eE]/);
+    var len = (eSplit[0].split(".")[1] || "").length - +(eSplit[1] || 0);
+    return len > 0 ? len : 0;
+}
+function float2Fixed(num) {
+    if (num.toString().indexOf("e") === -1) {
+        return Number(num.toString().replace(".", ""));
+    }
+    var dLen = digitLength(num);
+    return dLen > 0 ? strip(Number(num) * Math.pow(10, dLen)) : Number(num);
+}
+function times(num1, num2) {
+    var num1Changed = float2Fixed(num1);
+    var num2Changed = float2Fixed(num2);
+    var baseNum = digitLength(num1) + digitLength(num2);
+    var leftValue = num1Changed * num2Changed;
+    return leftValue / Math.pow(10, baseNum);
+}
 function add(value) {
-    return function (value1) {
-        var _a, _b;
-        if (typeof value1 !== "undefined") {
-            var digit1 = ((_a = value.toString().split(".")[1]) === null || _a === void 0 ? void 0 : _a.length) || 0;
-            var digit2 = ((_b = value1.toString().split(".")[1]) === null || _b === void 0 ? void 0 : _b.length) || 0;
-            var m = Math.pow(10, Math.max(digit1, digit2));
-            value = (Number(value) * m + Number(value1) * m) / m;
-            return arguments.callee;
+    return function secondaryAdd(secondaryValue) {
+        if (typeof secondaryValue !== "undefined") {
+            var baseNum = Math.pow(10, Math.max(digitLength(value), digitLength(secondaryValue)));
+            value = (times(value, baseNum) + times(secondaryValue, baseNum)) / baseNum;
+            return secondaryAdd;
         }
         else {
             return value;
@@ -15,11 +36,9 @@ function add(value) {
 }
 // 示例
 // add(1)("0.1")(0.1)()
-console.log(add(1)("0.1")(0.1)());
-// '1.2'
+console.log(add(1)("0.1")(0.1)()); // '1.2'
 // add(1)("ABC")()
-console.log(add(1)("ABC")());
-// 'NaN'
+console.log(add(1)("ABC")()); // 'NaN'
 // add("1.1e+4")("-1")()
-console.log(add("1.1e+4")("-1")());
-// '10999'
+console.log(add("1.1e+4")("-1")()); // '10999'
+console.log(add("1.000000000000000000000000000000000000000000000000001")(0.1)());
--- float-calculator/float-calculator.ts
+++ float-calculator/float-calculator.ts
@@ -1,11 +1,40 @@
-function add(value?: number | string) {
-  return function (value1?: number | string): any {
-    if (typeof value1 !== "undefined") {
-      let digit1 = value.toString().split(".")[1]?.length || 0
-      let digit2 = value1.toString().split(".")[1]?.length || 0
-      let m = 10 ** Math.max(digit1, digit2)
-      value = (Number(value) * m + Number(value1) * m) / m
-      return arguments.callee
+"use strict"
+
+type numType = number | string
+
+function strip(num: numType, precision = 15): number {
+  return +parseFloat(Number(num).toPrecision(precision))
+}
+
+function digitLength(num: numType): number {
+  const eSplit = num.toString().split(/[eE]/)
+  const len = (eSplit[0].split(".")[1] || "").length - +(eSplit[1] || 0)
+  return len > 0 ? len : 0
+}
+
+function float2Fixed(num: numType): number {
+  if (num.toString().indexOf("e") === -1) {
+    return Number(num.toString().replace(".", ""))
+  }
+  const dLen = digitLength(num)
+  return dLen > 0 ? strip(Number(num) * Math.pow(10, dLen)) : Number(num)
+}
+
+function times(num1: numType, num2: numType) {
+  const num1Changed = float2Fixed(num1)
+  const num2Changed = float2Fixed(num2)
+  const baseNum = digitLength(num1) + digitLength(num2)
+  const leftValue = num1Changed * num2Changed
+
+  return leftValue / Math.pow(10, baseNum)
+}
+
+function add(value?: numType) {
+  return function secondaryAdd(secondaryValue?: numType): any {
+    if (typeof secondaryValue !== "undefined") {
+      const baseNum = Math.pow(10, Math.max(digitLength(value), digitLength(secondaryValue)))
+      value = (times(value, baseNum) + times(secondaryValue, baseNum)) / baseNum
+      return secondaryAdd
     } else {
       return value
     }
@@ -14,11 +43,10 @@ function add(value?: number | string) {
 
 // 示例
 // add(1)("0.1")(0.1)()
-console.log(add(1)("0.1")(0.1)())
-// '1.2'
+console.log(add(1)("0.1")(0.1)()) // '1.2'
 // add(1)("ABC")()
-console.log(add(1)("ABC")())
-// 'NaN'
+console.log(add(1)("ABC")()) // 'NaN'
 // add("1.1e+4")("-1")()
-console.log(add("1.1e+4")("-1")())
-// '10999'
+console.log(add("1.1e+4")("-1")()) // '10999'
+
+console.log(add("1.000000000000000000000000000000000000000000000000001")(0.1)())
