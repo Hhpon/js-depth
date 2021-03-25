2ccf850405f2823e715eed0b983b382c2d725aab
hhp <624791164@qq.com>
2021-03-25 20:17:22 +0800

    学习期约内容-then方法规范

---
+++ 期约/熟悉期约使用规则/then.js
@@ -0,0 +1,34 @@
+const p = new Promise((resolve, reject) => {
+  const time = new Date().getTime()
+  console.log(time)
+  if (time % 2 === 0) {
+    resolve(time)
+  } else {
+    reject(time)
+  }
+})
+
+const p1 = p.then(
+  (res) => {
+    console.log(res)
+    // return Promise.reject("等待之后成功啦")
+    return new Promise((resolve) => {
+      resolve("等待之后成功啦")
+    })
+  },
+  (err) => {
+    console.log(err)
+  }
+)
+
+setTimeout(() => {
+  console.log(p1)
+  p1.then(
+    (result) => {
+      console.log("promise状态是:成功 " + result)
+    },
+    (err) => {
+      console.log("promise状态是:失败 " + err)
+    }
+  )
+}, 0)
