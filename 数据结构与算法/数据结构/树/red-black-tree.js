8206f50a7ca8124fce0cac02124cf9102c1cb7db
Hhpon <624791164@qq.com>
2021-04-05 12:46:32 +0800

    学习红黑树

---
+++ 数据结构与算法/数据结构/树/red-black-tree.js
@@ -0,0 +1,140 @@
+const { Compare } = require("../util")
+
+const Colors = {
+  RED: "red",
+  BLACK: "black",
+}
+
+class RedBlackNode extends Node {
+  constructor(key) {
+    super(key)
+    this.key = key
+    this.color = Colors.RED
+    this.parent = null
+  }
+
+  isRed() {
+    return this.color === Colors.RED
+  }
+}
+
+class RedBlackTree extends BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    super(compareFn)
+    this.root = null
+  }
+
+  insert(key) {
+    if (this.root === null) {
+      this.root = new RedBlackNode(key)
+      this.root.color = Colors.BLACK
+    } else {
+      const newNode = this.insertNode(this.root, key)
+      this.fixTreeProperties(newNode)
+    }
+  }
+
+  insertNode(node, key) {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left === null) {
+        node.left = new RedBlackNode(key)
+        node.left.parent = node
+        return node.left
+      } else {
+        return this.insertNode(node.left, key)
+      }
+    } else if (node.left === null) {
+      node.right = new RedBlackNode(key)
+      node.right.parent = node
+      return node.right
+    } else {
+      return this.insertNode(node.right, key)
+    }
+  }
+
+  fixTreeProperties(node) {
+    while (node && node.parent && node.parent.color.isRed() && node.color !== Colors.BLACK) {
+      let parent = node.parent
+      const grandParent = parent.parent
+      if (grandParent && grandParent.left === parent) {
+        const uncle = grandParent.right
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED
+          parent.color = Colors.BLACK
+          uncle.color = Colors.BLACK
+          node = grandParent
+        } else {
+          if (node === parent.right) {
+            this.rotationRR(parent)
+            node = parent
+            parent = node.parent
+          }
+          this.rotationLL(grandParent)
+          parent.color = Colors.BLACK
+          grandParent.color = Colors.RED
+          node = parent
+        }
+      } else {
+        const uncle = grandParent.left
+
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED
+          parent.color = Colors.BLACK
+          uncle.color = Colors.BLACK
+          node = grandParent
+        } else {
+          if (node === parent.left) {
+            this.rotationLL(parent)
+            node = parent
+            parent = node.parent
+          }
+          this.rotationRR(grandParent)
+          parent.color = Colors.BLACK
+          grandParent.color = Colors.RED
+          node = parent
+        }
+      }
+    }
+    this.root.color = Colors.BLACK
+  }
+
+  rotationLL(node) {
+    const tmp = node.left
+    node.left = tmp.right
+    if (tmp.right && tmp.right.key) {
+      tmp.right.parent = node
+    }
+    tmp.parent = node.parent
+    if (!node.parent) {
+      this.root = tmp
+    } else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp
+      } else {
+        node.parent.right = tmp
+      }
+    }
+    tmp.right = node
+    node.parent = tmp
+  }
+
+  rotationRR(node) {
+    const tmp = node.right
+    node.right = tmp.left
+    if (tmp.left && tmp.left.key) {
+      tmp.left.parent = node
+    }
+    tmp.parent = node.parent
+    if (!node.parent) {
+      this.root = tmp
+    } else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp
+      } else {
+        node.parent.right = tmp
+      }
+    }
+    tmp.left = node
+    node.parent = tmp
+  }
+}
