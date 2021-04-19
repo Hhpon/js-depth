619a655b5624e4cf4b02688b708796193207a2ba
hhp <624791164@qq.com>
2021-04-19 18:41:59 +0800

    迭代方法实现斐波那契数列

--- 数据结构与算法/算法/递归/fibonacciIterative.js
+++ 数据结构与算法/算法/递归/fibonacciIterative.js
@@ -8,4 +8,23 @@ function fibonacciIterative(n) {
   return fibonacciIterative(n - 1) + fibonacciIterative(n - 2)
 }
 
-console.log(fibonacciIterative(5))
+function fibonacciIterative1(n) {
+  if (n <= 0) {
+    return 0
+  }
+  if (n <= 1) {
+    return 1
+  }
+  let f0 = 0,
+    f1 = 1
+  let result = 0
+  for (let i = 2; i <= n; i++) {
+    result = f0 + f1
+    f0 = f1
+    f1 = result
+  }
+  return result
+}
+
+console.log(fibonacciIterative(10))
+console.log(fibonacciIterative1(10))
