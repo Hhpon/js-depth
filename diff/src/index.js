0977f191ff4a5a86c01b98a0fb40efb202c3e51e
Hhpon <624791164@qq.com>
2022-03-23 22:13:38 +0800

    update diff

--- diff/src/index.js
+++ diff/src/index.js
@@ -3,13 +3,16 @@ import patch from "./mySnabbdom/patch"
 
 // 获取dom节点
 const container = document.getElementById("container")
+const btn = document.getElementById('btn')
 
 // const myVnode1 = h("h1", {}, "你好")
-const myVnode1 = h("ul", {}, [h("li", {}, "A"), h("li", {}, "B"), h("li", {}, ["C"]), h("li", {}, "D")])
+const myVnode1 = h("ul", {}, '[h("li", {}, "A"), h("li", {}, "B"), h("li", {}, ["C"]), h("li", {}, "D")]')
 
 // 第一次上树
 patch(container, myVnode1)
 
 const myVnode2 = h("ul", {}, [h("li", {}, "A"), h("li", {}, "B"), h("li", {}, ["C"])])
 
-patch(myVnode1, myVnode2)
+btn.onclick = function () {
+  patch(myVnode1, myVnode2)
+}
--- diff/src/mySnabbdom/patch.js
+++ diff/src/mySnabbdom/patch.js
@@ -1,3 +1,4 @@
+import { vnode } from "snabbdom";
 import createElement from "./createElement"
 import vNode from "./vNode"
 
@@ -9,7 +10,29 @@ export default function (oldVnode, newVnode) {
 
   if (oldVnode.key === newVnode.key && oldVnode.sel === newVnode.sel) {
     // 是同一个节点
-    console.log("这好像是同一个节点")
+    // 判断新旧vnode是否是同一个对象
+    if (oldVnode === newVnode) return
+    // 判断新vnode是否含有text属性
+    if (newVnode.text !== undefined && newVnode.children == undefined || newVnode.children.length === 0) {
+      console.log('newVnode 有text 属性');
+      if (newVnode.text !== oldVnode.text) {
+        // 如果新虚拟节点中的text和虚拟节点的text不同，那么直接让新的text写入老的elm中即可，老的vnode中的children会自动消失
+        oldVnode.elm.innerText = newVnode.text
+      }
+    } else {
+      console.log('newVnode 没有text 属性');
+      // 判断老的有没有children
+      if (oldVnode.children != undefined && oldVnode.children.length > 0) {
+        // 老的有children，此时就是最复杂的情况，新老都有children
+      } else {
+        // 老的没有children
+        // 清空老节点的内容
+        oldVnode.elm.innerHTML = ''
+        for (let i = 0; i < newVnode.children.length; i++) {
+          oldVnode.elm.appendChild(createElement(newVnode.children[i]))
+        }
+      }
+    }
   } else {
     // 不是同一个节点，插入新的，暴力删除原来的
     const newVnodeElm = createElement(newVnode)
