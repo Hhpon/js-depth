69e8c67c32608b4f326e1111b4ed5ca72540fbf0
Hhpon <624791164@qq.com>
2022-09-19 16:55:00 +0800

    init vue-demo

---
+++ vue2.0/vue-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    hello world
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      a: 1,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
